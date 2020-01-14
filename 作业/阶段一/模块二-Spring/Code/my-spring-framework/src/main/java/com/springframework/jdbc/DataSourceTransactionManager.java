79a4a3f990f3a80b2c20dcb0ba6298e5c3f84995
zhudan <dan.zhu@aliyun.com>
2020-01-14 10:04:18 +0800

    spring

--- 作业/阶段一/模块二-Spring/Code/my-spring-framework/src/main/java/com/springframework/jdbc/DataSourceTransactionManager.java
+++ 作业/阶段一/模块二-Spring/Code/my-spring-framework/src/main/java/com/springframework/jdbc/DataSourceTransactionManager.java
@@ -14,21 +14,6 @@ public class DataSourceTransactionManager {
         this.dataSource = dataSource;
     }
 
-//    // 当前线程的连接
-//    private ThreadLocal<Connection> threadLocal = new ThreadLocal<>();
-//
-//    public Connection getCurrentThreadConn() throws SQLException {
-//        // 判断当前线程中是否已经绑定连接，如果没有绑定，需要从连接池获取一个连接绑定到当前线程
-//        Connection connection = threadLocal.get();
-//        if(connection == null){
-//            // 从连接池拿连接并绑定到线程
-//            connection = dataSource.getConnection();
-//            // 绑定到当前线程
-//            threadLocal.set(connection);
-//        }
-//        return connection;
-//    }
-
     // 开启手动事务控制
     public void beginTransaction(JoinPoint joinPoint) {
         try {
